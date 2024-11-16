661c35b5979932c5d3baeb501dd898cca0b134b6
ashubhankhar <157944297+ashubhankhar@users.noreply.github.com>
2024-11-16 16:59:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "26737035")
     API_HASH  = os.environ.get("API_HASH", "c75ebfd9da233fcc99b6e2f86e42cf0b")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6755031145:AAHOV-t9LiYiLFry8uZs5zljapVDowNrHrw") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6592959198:AAFTw_Ke0gb2bW2WGd9TVkrGfKCBTzNUOvw") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
@@ -39,7 +39,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6706180358').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "Dramafilez") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1002242540576"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","0"))
     MAX_CONCURRENT_TRANSMISSIONS = int(os.environ.get("MAX_CONCURRENT_TRANSMISSIONS", "2")) # Set the maximum amount of concurrent transmissions (uploads & downloads).
     
     # wes response configuration     
